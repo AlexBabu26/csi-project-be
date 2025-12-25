f2271846064712308bba4e16931967dd896ebf92
AlexBabu26 <alexbabu1998@gmail.com>
2025-12-25 22:10:33 +0530

    Add migration script for merging scoring columns
    Created a new migration file `23693695c068_merge_scoring_columns_branch.py` to facilitate the merging of scoring columns. The upgrade and downgrade functions are currently placeholders for future implementation.

---
+++ alembic/versions/23693695c068_merge_scoring_columns_branch.py
@@ -0,0 +1,19 @@
+"""merge_scoring_columns_branch"""
+
+revision = '23693695c068'
+down_revision = ('5a7b8c9d0e1f', 'e1f2g3h4i5j6')
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
+
